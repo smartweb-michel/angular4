a058816b3c6790fe73289f8d4512f664394bbdcf
zkrav786 <zkrav786@gmail.com>
2018-06-28 08:52:41 +0800

    update app

--- an4/src/main.ts
+++ an4/src/main.ts
@@ -8,4 +8,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+// tslint:disable-next-line
+  .catch(err => console.log(err));
