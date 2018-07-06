6b3290ecb0eeb3087444fea8a8d8423fa0d7f90f
zkrav786 <zkrav786@gmail.com>
2018-07-06 08:57:09 +0800

    setup app module

--- an4/src/app/app.module.ts
+++ an4/src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './routes';
+
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './stats/store/reducers/index';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { GetProductsEffect } from './stats/store/effects/getMarketData.effect';
+import { OrganisationEffect } from './stats/store/effects/organisation.effects';
+import { RepoEffect } from './stats/store/effects/repositorry.effect';
+import { MemberEffect } from './stats/store/effects/member.effect';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    SharedModule,
+    StoreModule.forRoot(reducers),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([GetProductsEffect, OrganisationEffect, RepoEffect, MemberEffect]),
   ],
   providers: [],
   bootstrap: [AppComponent]
