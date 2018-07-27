6c996b1e3d7cad04ba8aef26dfdb7ef78f2ff5ab
zkrav786 <zkrav786@gmail.com>
2018-07-27 09:04:14 +0800

    Create routes.ts

---
+++ an4/src/app/organization/list/routes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { ListComponent } from './list.component';
+
+export const routes: Route[] = [
+    {
+        path: '',
+        component: ListComponent
+      }
+  ];
