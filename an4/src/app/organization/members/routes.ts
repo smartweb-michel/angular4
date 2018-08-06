ff019b5b35b10052551cc9a56b2b4d291c9db677
zkrav786 <zkrav786@gmail.com>
2018-08-06 09:08:26 +0800

    Create routes.ts

---
+++ an4/src/app/organization/members/routes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { MembersComponent } from './members.component';
+
+export const routes: Route[] = [
+    {
+        path: '',
+        component: MembersComponent
+      }
+  ];
