6f16b15fb7a0b78f2b66887e7ce63ebad49664ad
zkrav786 <zkrav786@gmail.com>
2018-08-10 09:09:56 +0800

    Create routes.ts

---
+++ an4/src/app/organization/online/routes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { OnlineComponent } from './online.component';
+
+export const routes: Route[] = [
+    {
+        path: '',
+        component: OnlineComponent
+    }
+];
