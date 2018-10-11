581e82e4e8e3aeb038545c03e97ead3d1bbacc94
zkrav786 <zkrav786@gmail.com>
2018-10-11 09:49:01 +0800

    Update coins-datasource.service.ts

--- an4/src/app/organization/services/coins-datasource.service.ts
+++ an4/src/app/organization/services/coins-datasource.service.ts
@@ -118,7 +118,17 @@ export class CoinsDataSource extends DataSource<any> {
                     });
                 }
                 break;
-            
+            case 'volume':
+                if (this.sort.direction === 'desc') {
+                    result = this.dao.coins.value.sort((a, b) => {
+                        return b.finance.volume - a.finance.volume;
+                    });
+                } else {
+                    result = this.dao.coins.value.sort((a, b) => {
+                        return a.finance.volume - b.finance.volume;
+                    });
+                }
+                break;
             case 'changeDay':
                 if (this.sort.direction === 'desc') {
                     result = this.dao.coins.value.sort((a, b) => {
